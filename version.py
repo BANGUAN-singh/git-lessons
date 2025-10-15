f30094f8b330dac1c2014c8660846c61d1386a66
banguan singh merah <banguanmerah@gmail.com>
2025-10-15 16:22:59 +0300

    second commit or second commit or joan tategede

--- version.py
+++ version.py
@@ -1,5 +1,6 @@
 #this is a comment in my file written in git bash
 # touch is used to create a new file dont forget to add the file extension whereas  echo is used to write within a file, one > is used  for the first time then when adding to prevent overridding use double as such >> named  as appending
 print ("this is a string")
-print ("jon mukodo")
+print ("joan mukodo")
+print ("joan tabitegede asaba mutwalo")
 
