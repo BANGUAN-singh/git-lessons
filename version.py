9abbcfe4dada491eac79b0d4588130394eded36f
banguan singh merah <banguanmerah@gmail.com>
2025-10-15 16:16:35 +0300

    initial commit or first commit

---
+++ version.py
@@ -0,0 +1,5 @@
+#this is a comment in my file written in git bash
+# touch is used to create a new file dont forget to add the file extension whereas  echo is used to write within a file, one > is used  for the first time then when adding to prevent overridding use double as such >> named  as appending
+print ("this is a string")
+print ("jon mukodo")
+
